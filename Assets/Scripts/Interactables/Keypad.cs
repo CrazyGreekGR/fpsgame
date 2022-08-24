b6215d019f2995605d9e94b731f7d4ee92c73257
CrazyGreekGR <vagenasargyrhs@gmail.com>
2022-08-24 10:29:02 +0300

    Added Movement, Raycast, and Interactables Commited the correct project lol

---
+++ Assets/Scripts/Interactables/Keypad.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keypad : Interactable
+{
+    [SerializeField]
+    private GameObject door;
+    private bool doorOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected override void Interact()
+    {
+        doorOpen =! doorOpen;
+        if(doorOpen == true)
+        {
+            door.SetActive(false);
+        }
+        else
+        {
+            door.SetActive(true);
+        }
+    }
+}
